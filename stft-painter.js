7dd5e17bbdffdbfbbd8b5943a770337db4b01280
dy <df.creative@gmail.com>
2019-01-27 14:31:43 -0500

    Add painter!

--- stft-painter.js
+++ stft-painter.js
@@ -1,15 +1,32 @@
 import Waveform from 'gl-waveform'
 import {on, off, emit} from 'emmy'
+import Writer from 'web-audio-write'
+import OO from 'ooura'
 
 let wf = Waveform()
 
+// FIXME: this value
+let write = Writer({samplesPerFrame: 256})
+
+
 const FFT_SIZE = 512
 let spectrum = new Float64Array(FFT_SIZE)
-spectrum[100] = 1
-wf.set(spectrum)
-wf.render()
+spectrum[2] = 1
+wf.set(spectrum).render()
 
-var isPressed = false
+// Set up the FFT
+let oo = new OO(spectrum.length, {"type":"real", "radix":4});
+let output = oo.scalarArrayFactory();
+let re = oo.vectorArrayFactory();
+let im = oo.vectorArrayFactory();
+
+write(re, function tick() {
+	oo.ifft(output.buffer, spectrum.buffer, im.buffer);
+	// wf.push(output).render()
+	write(output, tick)
+})
+
+
 
 // drag
 on(wf.canvas, 'mousedown touchstart', e => {
@@ -32,14 +49,7 @@ on(wf.canvas, 'mousedown touchstart', e => {
 		lastX = e.x
 		lastY = e.y
 	})
-	on(wf.canvas, 'mouseup.draw touchend.draw', e => {
+	on(wf.canvas, 'mouseup.draw touchend.draw mouseleave.draw', e => {
 		off(wf.canvas, '.draw')
 	})
 })
-
-
-
-
-function ifft(mags) {
-
-}
